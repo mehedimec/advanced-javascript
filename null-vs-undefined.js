6204995476a6cf6653a43c7a13aebc450e7409e5
mehedimec <mehedimec@gmail.com>
2020-07-26 10:21:50 +0600

    null vs undefined clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+// Undefined Cases:
+
+// let pakhi;
+// console.log(pakhi);
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 52);
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+//     return
+// }
+// const result = add(13);
+// console.log(pakhi);
+
+// const student = {name:"Robert", class: 7};
+// console.log(student.father); //father property ta access korte chaitese but seta declare e kora hoy nai
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages);
